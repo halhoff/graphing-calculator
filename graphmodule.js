9c494071ad89faaadc471f2896974b0eb5de40ec
halhoff <halhoffmeyer@gmail.com>
2024-12-30 13:56:25 -0500

    added resizing

--- graphmodule.js
+++ graphmodule.js
@@ -11,68 +11,37 @@ function generateCoords(equation) {
             result = parseFloat(result.toFixed(5));
         }
         coords.push([x, result]);
-        console.log(x + ', ' + result);
         x += step;
     }
     return coords;
 }
 
 function drawAxes(ctx, width, height) {
-
-    clearGraph();
-
+    clearGraph(ctx);
     document.getElementById("answer").textContent = ``;
-
     ctx.beginPath();
 
-    // x-axis
-    ctx.moveTo(50, height / 2);
-    ctx.lineTo(width - 50, height / 2);
-
-    // y-axis
-    ctx.moveTo(width / 2, 50);
-    ctx.lineTo(width / 2, height - 50);
-
+    ctx.moveTo(0, height / 2);
+    ctx.lineTo(width, height / 2);
+    ctx.moveTo(width / 2, 0);
+    ctx.lineTo(width / 2, height);
     ctx.strokeStyle = 'white';
     ctx.lineWidth = 2;
     ctx.stroke();
 
-    // Draw x-axis arrow
-    ctx.moveTo(width - 60, height / 2 - 5);
-    ctx.lineTo(width - 50, height / 2);
-    ctx.lineTo(width - 60, height / 2 + 5);
-    ctx.stroke();
-
-    // Draw y-axis arrow
-    ctx.moveTo(width / 2 - 5, 60);
-    ctx.lineTo(width / 2, 50);
-    ctx.lineTo(width / 2 + 5, 60);
-    ctx.stroke();
-
-    // Add x-axis label
-    ctx.font = "16px Consolas";
+    ctx.font = "bold 16px Inter";
     ctx.fillStyle = "white";
-    ctx.fillText("x", width - 45, height / 2 + 5);
-
-    // Add y-axis label
-    ctx.fillText("y", width / 2 - 4, 40);
 }
 
 function drawCoordinates(ctx, coords, width, height) {
-
-    const scale = 50;
-
-    // Start drawing the path for the line
+    clearGraph(ctx);
+    const scale = width / (coords[coords.length - 1][0] - coords[0][0]);
     ctx.beginPath();
     let lastPoint = null;
-
-    // Set the fill style for the points
     ctx.fillStyle = 'aqua';
-
-    coords.forEach(([x, y]) => {
+    coords.forEach(([x, y]) => { // scale = width / -10
         const canvasX = width / 2 + x * scale;
         const canvasY = height / 2 - y * scale;
-
         if (y !== null) {
             if (lastPoint) {
                 ctx.moveTo(lastPoint.x, lastPoint.y);
@@ -87,15 +56,11 @@ function drawCoordinates(ctx, coords, width, height) {
             lastPoint = null;
         }
     });
-
-    // Stroke the line connecting the points
     ctx.strokeStyle = 'aqua';
     ctx.lineWidth = 3;
     ctx.stroke();
 }
 
-function clearGraph() {
-    const canvas = document.getElementById('graphCanvas');
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function clearGraph(ctx) {
+    ctx.clearRect(0, 0, graph.width, graph.height);
 }
--- main.js
+++ main.js
@@ -1,4 +1,41 @@
+var PIXEL_RATIO = (function () {
+    var ctx = document.createElement("canvas").getContext("2d"),
+        dpr = window.devicePixelRatio || 1,
+        bsr = ctx.webkitBackingStorePixelRatio ||
+              ctx.mozBackingStorePixelRatio ||
+              ctx.msBackingStorePixelRatio ||
+              ctx.oBackingStorePixelRatio ||
+              ctx.backingStorePixelRatio || 1;
+
+    return dpr / bsr;
+})();
+
+createHiDPICanvas = function(w, h, ratio) {
+    if (!ratio) { ratio = PIXEL_RATIO; }
+    var can = document.createElement("canvas");
+    can.width = w * ratio;
+    can.height = h * ratio;
+    can.style.width = w + "px";
+    can.style.height = h + "px";
+    can.getContext("2d").setTransform(ratio, 0, 0, ratio, 0, 0);
+    return can;
+}
+
+const SCREEN_HEIGHT = window.innerWidth / 4;
+const SCREEN_WIDTH = window.innerWidth / 4;
+
+const graph = createHiDPICanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+graph.id = 'graph';
+const ctx = graph.getContext('2d');
+document.getElementById('graph-canvas').appendChild(graph);
+
+const axes = createHiDPICanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+axes.id = 'axes';
+const ctx_axes = axes.getContext('2d');
+document.getElementById('graph-canvas').append(axes);
+
 function equationOrExpression(expression) {
+    drawAxes(ctx_axes, SCREEN_WIDTH, SCREEN_HEIGHT);
     let equation = 0;
     for (let i = 0; i < expression.length; ++i) {
         if (expression[i] === 'x') {
@@ -7,10 +44,7 @@ function equationOrExpression(expression) {
         }
     }
     if (equation) {
-        const canvas = document.getElementById('graphCanvas');
-        const ctx = canvas.getContext('2d');
-        drawAxes(ctx, 550, 550);
-        drawCoordinates(ctx, generateCoords(expression), 550, 550);
+        drawCoordinates(ctx, generateCoords(expression), SCREEN_WIDTH, SCREEN_HEIGHT);
     }
     else {
         calculate(expression, 1);
